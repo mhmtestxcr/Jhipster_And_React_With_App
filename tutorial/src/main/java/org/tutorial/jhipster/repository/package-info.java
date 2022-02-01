54056a17fbffc5ab699f364e019f6b9e229cf3a8
Muhammet Esat <mhmtestxcr@gmail.com>
2022-02-01 22:59:39 +0300

    Initial version of tutorial generated by generator-jhipster@7.6.0

---
+++ tutorial/src/main/java/org/tutorial/jhipster/repository/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data JPA repositories.
+ */
+package org.tutorial.jhipster.repository;
---
+++ tutorial/src/test/java/org/tutorial/jhipster/TechnicalStructureTest.java
@@ -0,0 +1,27 @@
+package org.tutorial.jhipster;
+
+import static com.tngtech.archunit.base.DescribedPredicate.alwaysTrue;
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.belongToAnyOf;
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+
+import com.tngtech.archunit.core.importer.ImportOption.DoNotIncludeTests;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.junit.ArchTest;
+import com.tngtech.archunit.lang.ArchRule;
+
+@AnalyzeClasses(packagesOf = TutorialApp.class, importOptions = DoNotIncludeTests.class)
+class TechnicalStructureTest {
+
+    // prettier-ignore
+    @ArchTest
+    static final ArchRule respectsTechnicalArchitectureLayers = layeredArchitecture()
+        .layer("Config").definedBy("..config..")
+        .layer("Web").definedBy("..web..")
+        .layer("Security").definedBy("..security..")
+
+        .whereLayer("Config").mayNotBeAccessedByAnyLayer()
+        .whereLayer("Web").mayOnlyBeAccessedByLayers("Config")
+        .whereLayer("Security").mayOnlyBeAccessedByLayers("Web", "Config")
+
+        .ignoreDependency(belongToAnyOf(TutorialApp.class), alwaysTrue());
+}
